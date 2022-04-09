bcea0ec65e0c99499ac04b463c8e6bcfbbab3bd5
Gilbert Oliveira <gilberticm8@gmail.com>
2022-04-09 14:23:05 -0300

    Modified top navigation

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,15 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Products') }}
+                    </x-jet-nav-link>
+
+                    <x-jet-nav-link href="">
+                        {{ __('About') }}
+                    </x-jet-nav-link>
+
+                    <x-jet-nav-link href="">
+                        {{ __('Contact ') }}
                     </x-jet-nav-link>
                 </div>
             </div>
